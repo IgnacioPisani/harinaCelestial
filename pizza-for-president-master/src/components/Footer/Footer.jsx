36ada8cee791d6f81a497931f5c7995204fa6d63
Ignacio Pisani <69583470+IgnacioPisani@users.noreply.github.com>
2024-10-08 18:59:20 -0300

    Se incluyeron los links de los iconos

--- pizza-for-president-master/src/components/Footer/Footer.jsx
+++ pizza-for-president-master/src/components/Footer/Footer.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Logo from '../../images/g2m.png'
 import {
+	FaFacebook,
 	FaInstagram,
+	FaYoutube,
+	FaTwitter,
+	FaLinkedin,
 	FaWhatsapp,
 } from 'react-icons/fa';
 import {
@@ -8,13 +13,10 @@ import {
 	FooterWrap,
 	SocialMedia,
 	SocialMediaWrap,
-	SocialLogo,
+	SocialLogoImage,
 	SocialIcons,
 	SocialIconLink,
-	SocialImage,
-	SocialImageDiv,
 } from './FooterElements';
-import logo from '../../images/g2m.png';
 
 function Footer() {
 	return (
@@ -22,29 +24,26 @@ function Footer() {
 			<FooterWrap>
 				<SocialMedia>
 					<SocialMediaWrap>
-						<SocialImageDiv>
-					<SocialImage src={logo}/>
-					</SocialImageDiv>
-					<SocialIcons>
-						<h1>Contactos:</h1>
+					<SocialLogoImage >
+							<img src={Logo} alt='Logo' />
+						</SocialLogoImage>						
+						<SocialIcons>
 							<SocialIconLink
-								href='/'
-								target='_blank'
-								aria-label='Instagram'
-								rel='noopener noreferrer'
-							>
-								<FaInstagram />
-							</SocialIconLink>
-							<SocialIconLink
-								href='/'
+								href='https://api.whatsapp.com/send/?phone=5493515165078&text&type=phone_number&app_absent=0'
 								target='_blank'
 								aria-label='Whatsapp'
 								rel='noopener noreferrer'
 							>
 								<FaWhatsapp />
 							</SocialIconLink>
-						
-						
+							<SocialIconLink
+								href='https://www.instagram.com/g2m.celestial/'
+								target='_blank'
+								aria-label='Instagram'
+								rel='noopener noreferrer'
+							>
+								<FaInstagram />
+							</SocialIconLink>
 						</SocialIcons>
 					</SocialMediaWrap>
 				</SocialMedia>
--- pizza-for-president-master/src/components/Footer/FooterElements.jsx
+++ pizza-for-president-master/src/components/Footer/FooterElements.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const FooterContainer = styled.footer`
-	background-color: #ff8806;
+	background-color: #FD8B06;
 `;
 
 export const FooterWrap = styled.div`
@@ -32,6 +32,17 @@ export const SocialMediaWrap = styled.div`
 	} ;
 `;
 
+export const SocialLogoImage = styled.div`
+	display: flex;
+	align-items: center;
+	margin-bottom: 16px;
+
+	img {
+		width: 100px; /* Ajusta el tamaño de la imagen según sea necesario */
+		height: auto;
+	}
+`;
+
 export const SocialLogo = styled(Link)`
 	color: #fff;
 	justify-self: start;
@@ -43,25 +54,15 @@ export const SocialLogo = styled(Link)`
 	margin-bottom: 16px;
 	font-weight: bold;
 `;
-export const SocialImageDiv = styled.div`
-    width: 30%;
-`;
-export const SocialImage = styled.img`
-	display: block; /* Cambiado a block para que ocupe todo el ancho disponible */
-	margin-left: 0; /* Asegura que no haya margen izquierdo */
-	margin-right: auto; /* Empuja el margen derecho para alinear a la izquierda */
-	width: 200px; /* Ancho fijo para la imagen */
-`;
 
 export const SocialIcons = styled.div`
-    display: flex;
-    flex-direction: column; /* Cambiado a columna para que el título esté encima de los iconos */
-    align-items: flex-end; /* Alinea los iconos a la derecha */
-    width: 70%; 
-    margin-left: auto; /* Asegura que se empuje a la derecha */
+	display: flex;
+  justify-content: space-evenly; 
+	align-items: center;
+	width: 240px;
 `;
 
 export const SocialIconLink = styled.a`
-    color: #fff;
-    font-size: 60px; /* Mantén el tamaño según lo necesites */
+	color: #fff;
+	font-size: 30px;
 `;
