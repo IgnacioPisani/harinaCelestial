ea93b96332740ad7ec513c9e5eefd08035cdb6d8
Ignacio Pisani <69583470+IgnacioPisani@users.noreply.github.com>
2024-10-27 19:21:04 -0300

    Update App.js

--- pizza-for-president-master/src/App.js
+++ pizza-for-president-master/src/App.js
@@ -3,13 +3,9 @@ import { GlobalStyle } from './globalStyles';
 import Home from './pages/Home/Home'; // Importa Home desde su archivo correspondiente
 import Footer from './components/Footer/Footer';
 import AboutUs from './pages/AboutUs/AboutUs';
-<<<<<<< HEAD
 import Faq from './pages/Faq/Faq';
-
-=======
 import NavBar from './components/Navbar/Navbar';
 import React, { useState } from 'react';
->>>>>>> 2e6e1880cea9f32a7ec235940b1ae64f69c67ebd
 
 function App() {
 	const [isOpen, setIsOpen] = useState(false);
@@ -28,11 +24,8 @@ function App() {
 				{/* Otra ruta sin los elementos de Home */}
 				<Route path="/about" element={<AboutUs/>} />
 
-				<Route path="/faq" element={<AboutUs/>} />
+				<Route path="/faq" element={<Faq/>} />
 				
-				{/* Puedes agregar más rutas aquí */}
-
-				<Route path="/Faq" element={<Faq/>} />
 			</Routes>
 			<Footer />
 		</Router>
