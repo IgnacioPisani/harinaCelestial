172b8215701c65f35dfdaa6969f70b656f3dcdb2
Ignacio Pisani <69583470+IgnacioPisani@users.noreply.github.com>
2024-12-19 11:24:28 -0300

    commit carousels

--- g2m/src/components/Carousel/Carousel.jsx
+++ g2m/src/components/Carousel/Carousel.jsx
@@ -4,7 +4,6 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./Carousel.css";
 
-// Flecha personalizada para el carrusel
 const Arrow = ({ className, style, onClick, direction }) => (
   <div
     className={`${className} custom-arrow ${direction}`}
@@ -17,20 +16,22 @@ const Arrow = ({ className, style, onClick, direction }) => (
 
 const Carousel = ({ data }) => {
   const [loadedImages, setLoadedImages] = useState({});
+  const [autoplay, setAutoplay] = useState(true);
 
   const handleImageLoad = (index) => {
     setLoadedImages((prev) => ({ ...prev, [index]: true }));
   };
+
   const settings = {
-    infinite: true, // Carrusel infinito
-    speed: 500, // Velocidad de transición
-    slidesToShow: 3, // Número de elementos visibles
-    slidesToScroll: 1, // Número de elementos a desplazar
-    autoplay: true, // Activar reproducción automática
-    autoplaySpeed: 2000, // Velocidad de reproducción automática
-    pauseOnHover: false, // No pausar al pasar el mouse
-    draggable: false, // Desactivar el arrastre
-    swipe: false, // Desactivar swipe en dispositivos táctiles
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    pauseOnHover: true, // Pausa el carrusel al hacer hover
+    draggable: false,
+    swipe: false,
     centerMode: true,
     lazyLoad: "ondemand",
     prevArrow: <Arrow direction="prev" />,
@@ -45,29 +46,37 @@ const Carousel = ({ data }) => {
       },
     ],
   };
+  
 
   return (
     <div className="carousel">
-    <Slider {...settings}>
-  {data.map((product, index) => (
-    <div
-      key={index}
-      className={`product-card ${loadedImages[index] ? "loaded" : "loading"}`}
-    >
-      <img
-        src={product.img}
-        alt={product.alt}
-        onLoad={() => handleImageLoad(index)}
-        style={{ display: loadedImages[index] ? "block" : "none" }}
-      />
-      {!loadedImages[index] && (
-        <div className="image-placeholder">Cargando...</div>
-      )}
-      <h3 style={{ fontWeight: "bold" }}>{product.name}</h3>
-    </div>
-  ))}
-</Slider>
-
+      <Slider
+        {...settings}
+        beforeChange={() => setAutoplay(true)}
+        afterChange={() => setAutoplay(true)}
+      >
+        {data.map((product, index) => (
+          <div
+            key={index}
+            className={`product-card ${loadedImages[index] ? "loaded" : "loading"}`}
+            onMouseEnter={() => setAutoplay(false)}
+            onMouseLeave={() => setAutoplay(true)}
+          >
+            <a href={product.link} >
+              <img
+                src={product.img}
+                alt={product.alt}
+                onLoad={() => handleImageLoad(index)}
+                style={{ display: loadedImages[index] ? "block" : "none" }}
+              />
+              {!loadedImages[index] && (
+                <div className="image-placeholder">Cargando...</div>
+              )}
+              <h3 style={{ fontWeight: "bold" }}>{product.name}</h3>
+            </a>
+          </div>
+        ))}
+      </Slider>
     </div>
   );
 };
